5a0d535dc7c316431ae3e17120c5d8fb80d4cddf
MadisonViaLux <madison.s.mckown@gmail.com>
2019-05-21 18:00:07 -0700

    nearly done

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -55,13 +55,13 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
-const fullName = [];
+// const fullName = [];
 
-for(let i = 0; i < runners.length; i++){
+// for(let i = 0; i < runners.length; i++){
+//     fullName.push(`${runners[i].first_name} ${runners[i].last_name}`);
+// }
 
-    fullName.push(`${runners[i].first_name} ${runners[i].last_name}`);
-}
-// runners.forEach(placeHolder => console.log(`${placeHolder.first_name} ${placeHolder.last_name}`))
+const fullName = runners.forEach(placeHolder => console.log(`${placeHolder.first_name} ${placeHolder.last_name}`));
 
 console.log(fullName);
 
@@ -69,21 +69,30 @@ console.log(fullName);
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 // let allCaps = [];
 
-const allCaps = runners.map(place => place.first_name.toUpperCase());
+const allCaps = runners.map(placeHolder => placeHolder.first_name.toUpperCase());
 
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+// let largeShirts = [];
 
-
+const largeShirts = runners.filter(placeHolder => placeHolder.shirt_size === "L" );
 
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+// let ticketPriceTotal = [];
+
+// const totalLandArea = cityData.reduce(function(banana, currentValue) {
+//   debugger;
+//   return banana + currentValue.population;
+  
+// }, 0);
+
+const ticketPriceTotal = runners.reduce((addLot, placeHolder) => addLot + placeHolder.donation, 0)
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
